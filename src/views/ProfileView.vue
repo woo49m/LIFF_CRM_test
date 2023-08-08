9b563f13959d04af832eec69de63d8bd73020d18
Ian Hsu <woo49m@gmail.com>
2023-08-08 17:21:28 +0800

    commit

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,2 +1,19 @@
 <template>Profile</template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import liff from "@line/liff";
+import { ref } from "vue";
+
+liff
+  .init({ liffId: "2000346384-BmLX7K5g" })
+  .then(() => {
+    console.log("初始化成功");
+    if (liff.isLoggedIn()) {
+      console.log("登入");
+    } else {
+      console.log("未登入");
+    }
+  })
+  .catch(() => {
+    console.log("初始化失敗");
+  });
+</script>
