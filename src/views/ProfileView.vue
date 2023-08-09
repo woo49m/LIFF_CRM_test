a5932a02c9596769c07ddd3b6e8a074ffb66bdae
Ian Hsu <woo49m@gmail.com>
2023-08-09 10:56:27 +0800

    commit

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -7,18 +7,11 @@ import liff from "@line/liff";
 import { ref } from "vue";
 
 const profile = ref<any>();
-liff
-  .init({ liffId: "2000346998-GNX6YwM7" })
-  .then(() => {
-    if (liff.isLoggedIn()) {
-      liff.getProfile().then((pro) => {
-        profile.value = pro;
-      });
-    } else {
-      console.log("未登入");
-    }
-  })
-  .catch(() => {
-    console.log("初始化失敗");
+if (liff.isLoggedIn()) {
+  liff.getProfile().then((pro) => {
+    profile.value = pro;
   });
+} else {
+  console.log("未登入");
+}
 </script>
