d2f8e4a4c0818231edd2837b946adb7de22df37c
Ian Hsu <woo49m@gmail.com>
2023-08-10 18:06:11 +0800

    test cognito

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -13,12 +13,27 @@ import { ref } from "vue";
 const picture_src = ref<string>("");
 const name = ref<string>("");
 const profile = ref<any>();
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
+
 if (liff.isLoggedIn()) {
   liff.getProfile().then((pro) => {
     profile.value = pro;
     name.value = pro.displayName;
     picture_src.value = pro.pictureUrl;
   });
+
+  const idToken = liff.getIDToken();
+  let COGNITO_ID = "COGNITO_ID"; // 'COGNITO_ID' has the format 'cognito-idp.REGION.amazonaws.com/COGNITO_USER_POOL_ID'
+  let loginData = {
+    [COGNITO_ID]: idToken,
+  };
+  const credentials = fromCognitoIdentityPool({
+    clientConfig: { region: REGION }, // Configure the underlying CognitoIdentityClient.
+    identityPoolId: "IDENTITY_POOL_ID",
+    logins: loginData,
+  });
+
+  console.log(credentials);
 } else {
   console.log("未登入");
 }
