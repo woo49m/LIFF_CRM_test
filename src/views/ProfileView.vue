fdf807dbfb80b2cecdaac2c2e538edb32ba61b32
Ian Hsu <woo49m@gmail.com>
2023-08-13 20:54:15 +0800

    ts

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -27,6 +27,22 @@ if (liff.isLoggedIn()) {
 
   const idToken = liff.getIDToken();
   const accessToken = liff.getAccessToken();
+
+  AWS.config.region = "ap-northeast-1";
+
+  // Configure the credentials provider to use your identity pool
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: "ap-northeast-1:9541a3a1-fc2a-456e-a161-95fae001efd7",
+    Logins: {
+      "access.line.me": accessToken,
+    },
+  });
+
+  AWS.config.getCredentials(() => {
+    console.log("Test Credential is get: ");
+    console.log(AWS.config);
+  });
+
   console.log(idToken);
   console.log(accessToken);
   AWS.config.credentials = new AWS.CognitoIdentityCredentials({
