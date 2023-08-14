c37ebe85f7cd9a5a0b72f68719847c7b49d78732
Ian Hsu <woo49m@gmail.com>
2023-08-14 09:57:27 +0800

    test

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -34,7 +34,7 @@ if (liff.isLoggedIn()) {
   AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: "ap-northeast-1:9541a3a1-fc2a-456e-a161-95fae001efd7",
     Logins: {
-      "https://access.line.me": idToken,
+      "access.line.me": idToken,
     },
   });
 
@@ -43,15 +43,6 @@ if (liff.isLoggedIn()) {
     console.log(AWS.config);
   });
 
-  console.log(idToken);
-  console.log(accessToken);
-  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: "ap-northeast-1:9541a3a1-fc2a-456e-a161-95fae001efd7",
-    Logins: {
-      "https://access.line.me": idToken,
-    },
-  });
-  console.log(AWS.config.credentials);
   console.log(
     fromCognitoIdentityPool({
       client: new CognitoIdentityClient({ region: "ap-northeast-1" }),
